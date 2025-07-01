b6ea2f4bfa9ffc7a6869da4999627fed804f23e8
Anonymushadow <claudelnahuel22@gmail.com>
2025-07-01 13:33:49 -0300

    v1.0.6

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Death Wolf Company',
+        short_name: 'DWC',
+        description: 'Portfolio Web de Death Wolf Company',
+        theme_color: '#4e0789',
+        icons: [
+          {
+            src: 'icons/PWA/DWC-Logo-Primario-912.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/PWA/DWC-Logo-Primario-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@components': '/src/components',  /* Cuando vite vean @components, lo va a reemplazar por /src/components */
