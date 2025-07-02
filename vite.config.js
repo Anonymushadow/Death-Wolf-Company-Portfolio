62fb2e79ecfbb09b08c2aa7e019560d7eb146d14
Anonymushadow <claudelnahuel22@gmail.com>
2025-07-02 03:09:41 -0300

    v1.0.7 - fix pwa mobile

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,34 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-     registerType: 'autoUpdate',
-      includeAssets: ['icons/PWA'], // Copia toda la carpeta directamente
-      manifest: false,
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#000000',
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/PWA'],
+      manifest: {
+        name: 'Death Wolf Company',
+        short_name: 'DWC',
+        description: 'Portfolio Web de Death Wolf Company',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#4e0789',
+        icons: [
+          {
+            src: 'icons/PWA/DWC-Logo-Primario-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/PWA/DWC-Logo-Primario-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
-  ],
-  resolve: {
-    alias: {
-      '@components': '/src/components',  /* Cuando vite vean @components, lo va a reemplazar por /src/components */
-      '@styles': '/src/styles',
-      '@config': '/src/config',
-      '@pages': '/src/pages',
-      '@hooks': '/src/hooks',
-      '@modules': '/src/modules',
-      '@contexts': "/src/contexts",
-      '@data': "/src/data"
-    }
-  }
+  ]
 })
