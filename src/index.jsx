8af4f6fd36b44ed8b59a348b3a8e27122229a3d4
Anonymushadow <claudelnahuel22@gmail.com>
2025-07-02 11:33:42 -0300

    v1.0.7 - Fix PWA

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { registerSW } from 'virtual:pwa-register'
 
 import { ThemeTransitionProvider } from '@contexts/TransitionThemeContext.jsx';
 import { LanguageProvider } from '@contexts/LanguageContext.jsx';
 import '@styles/base/styles.main.js';
 import { styles } from '@config/styles.js';
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
 
 styles.forEach((stylePath) => {
   const link = document.createElement('link');
