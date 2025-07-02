e9f45459e84acf5654afa96a389305d8b6810c9a
Anonymushadow <claudelnahuel22@gmail.com>
2025-07-02 12:12:20 -0300

    v1.0.7 - Fix PWA

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
@@ -8,28 +8,24 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: [
-        'icons/PWA/DWC-Logo-Primario-192.png',
-        'icons/PWA/DWC-Logo-Primario-512.png'
-      ],
       manifest: {
-        name: 'Death Wolf Company',
-        short_name: 'DWC',
-        description: 'Portfolio Web de Death Wolf Company',
-        theme_color: '#4e0789',
-        background_color: '#000000',
-        display: 'standalone',
-        start_url: '/',
+        name: "Death Wolf Company",
+        short_name: "DWC",
+        description: "Portfolio Web de Death Wolf Company",
+        theme_color: "#4e0789",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "/",
         icons: [
           {
-            src: '/DWC-Logo-Primario-192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/dwc-logo-primario-192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
           {
-            src: '/DWC-Logo-Primario-512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "/dwc-logo-primario-512.png",
+            sizes: "512x512",
+            type: "image/png"
           }
         ]
       },
@@ -40,14 +36,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@components': '/src/components',  /* Cuando vite vean @components, lo va a reemplazar por /src/components */
-      '@styles': '/src/styles',
-      '@config': '/src/config',
-      '@pages': '/src/pages',
-      '@hooks': '/src/hooks',
-      '@modules': '/src/modules',
-      '@contexts': "/src/contexts",
-      '@data': "/src/data"
+      "@components": "/src/components",  /* Cuando vite vean @components, lo va a reemplazar por /src/components */
+      "@styles": "/src/styles",
+      "@config": "/src/config",
+      "@pages": "/src/pages",
+      "@hooks": "/src/hooks",
+      "@modules": "/src/modules",
+      "@contexts": "/src/contexts",
+      "@data": "/src/data"
     }
   }
 })
