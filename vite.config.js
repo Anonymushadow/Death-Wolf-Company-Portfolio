ec004c5172a0ae6e4b21ac68245d3ee3597596a7
Anonymushadow <claudelnahuel22@gmail.com>
2025-07-02 04:52:17 -0300

    v1.0.7 - Fix PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plplugins: [
+  plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
